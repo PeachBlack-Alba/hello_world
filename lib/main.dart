b619aa284d94ff4e90339d6cf86bdee33bf7615f
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-04-26 16:17:18 +0200

    apunter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import "package:flutter/material.dart";
 
-void main() {}
+void main() {
+  runApp(
+      MyApp()); // runApp renders the widged we created (calls the build method in our widget). It's a feature from flutter package.
+}
 
-class MyApp extends StatelessWidget {}
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("Hello!"),
+    );
+  }
+}
