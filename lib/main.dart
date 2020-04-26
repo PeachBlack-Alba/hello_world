2b29941895bb0cc6b6262bc7b0983381e0df049a
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-04-26 17:48:21 +0200

    questions

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,34 @@ class MyApp extends StatelessWidget {
   // every widget needs to extend Stateless/full widget
   Widget build(BuildContext context) {
     //runApp tell flutter to render something in the screen, therefore we need a build method
+    var questions = [
+      "What/'s your favourite color?",
+      "What/'s your favourite city",
+    ];
     return MaterialApp(
       home: Scaffold(
         // scaffold is a widget imported from material.dart that gives a base page design to the app (ctrl + space to check all the possible arguments)
-        appBar: AppBar(title: Text("My First App")),
-        body: Text("This is my default text"),
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(
+          // <Widget>[] tells dart that the list is going to be a list of widgets
+          children: [
+            Text("The question"),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
