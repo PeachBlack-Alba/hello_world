6ee79a29772d9a70598c0f1a9eb49f6367bc12dc
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-04-26 17:06:17 +0200

    appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import "package:flutter/material.dart";
 
-void main() {
-  runApp(
-      MyApp()); // runApp renders the widged we created (calls the build method in our widget). It's a feature from flutter package.
-}
+void main() => runApp(
+    MyApp()); // runApp renders the widged we created (calls the build method in our widget). It's a feature from flutter package.
 
 class MyApp extends StatelessWidget {
+  @override //decorator from DART dependencies that makes code cleaner
+  // every widget needs to extend Stateless/full widget
   Widget build(BuildContext context) {
+    //runApp tell flutter to render something in the screen, therefore we need a build method
     return MaterialApp(
-      home: Text("Hello!"),
+      home: Scaffold(
+        // scaffold is a widget imported from material.dart that gives a base page design to the app (ctrl + space to check all the possible arguments)
+        appBar: AppBar(title: Text("My First App")),
+        body: Text("This is my default text"),
+      ),
     );
   }
 }
